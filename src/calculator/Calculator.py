8cdd959bced402ddb40e1bbf771414ce10964c63
jg-725 <72114483+jg-725@users.noreply.github.com>
2021-03-01 01:54:48 -0500

    Fix: Linked each method to its corresponding operation

--- src/calculator/Calculator.py
+++ src/calculator/Calculator.py
@@ -1,9 +1,4 @@
-from operations.Addition import addition
-from operations.Subtraction import subtraction
-from operations.Division import division
-from operations.Multiply import multiplication
-from operations.Square import square
-from operations.SquareRoot import root
+from OperationsFile.Operations import Operations
 
 
 class Calculator:
@@ -13,25 +8,25 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        self.result = addition(a, b)
+        self.result = Operations.addition(a, b)
         return self.result
 
     def subtract(self, a, b):
-        self.result = subtraction(a, b)
+        self.result = Operations.subtraction(a, b)
         return self.result
 
     def division(self, a, b):
-        self.result = division(a, b)
+        self.result = Operations.division(a, b)
         return self.result
 
     def multiply(self, a, b):
-        self.result = multiplication(a, b)
+        self.result = Operations.multiplication(a, b)
         return self.result
 
     def square(self, a):
-        self.result = square(a)
+        self.result = Operations.square(a)
         return self.result
 
     def root(self, a):
-        self.result = root(a)
+        self.result = Operations.root(a)
         return self.result
